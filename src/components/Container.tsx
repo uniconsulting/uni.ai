e9fa3d9888a497a85bd77100ba8ea55b48c85b15
uni.kit <uni.ai.consalting@gmail.com>
2026-02-19 06:49:26 +0400

    Update Container.tsx

--- src/components/Container.tsx
+++ src/components/Container.tsx
@@ -9,7 +9,7 @@ export function Container({ className, children }: Props) {
   return (
     <div
       className={cn(
-        'mx-auto w-full max-w-[1440px] px-4 md:px-8 lg:px-12',
+        "mx-auto w-full max-w-[1440px] px-4 md:px-8 lg:px-14",
         className
       )}
     >
