fcd5a9bceebef3bbe8b3d7a43e897c25f1b7de70
uni.kit <uni.ai.consalting@gmail.com>
2026-02-24 21:13:36 +0400

    Update Footer.tsx

--- src/components/Footer.tsx
+++ src/components/Footer.tsx
@@ -216,7 +216,7 @@ export function Footer() {
                     aria-hidden
                     className="pointer-events-none select-none absolute left-1/2 top-[62%] -translate-x-1/2 -translate-y-1/2"
                   >
-                    <div className="relative top-[4px] whitespace-nowrap text-bg/18 font-normal leading-none tracking-tight text-[94px]">
+                    <div className="relative top-[8px] whitespace-nowrap text-bg/18 font-normal leading-none tracking-tight text-[98px]">
                       衆志、城を成す
                     </div>
                   </div>
